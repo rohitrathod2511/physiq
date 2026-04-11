01b99e090accf9ee7690723edd47538493a4e2e4
RohitRathod9 <166100968+RohitRathod9@users.noreply.github.com>
2026-04-12 00:38:35 +0530

    Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.example.physiq"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -40,8 +40,8 @@ android {
         applicationId = "com.example.physiq"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
-        targetSdk = 35
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
